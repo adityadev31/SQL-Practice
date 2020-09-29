9d434e0cc440ae656a9db3aecff7307645a41625
AdiTya Dev <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-09-29 16:53:35 +0530

    Update solution_6_join.sql

--- solution_6_join.sql
+++ solution_6_join.sql
@@ -38,7 +38,9 @@ SELECT matchid, player FROM goal
 WHERE teamid = 'GER'
 
 
-
+/* 2 -> From the previous query you can see that Lars Bender's scored a goal in game 1012. Now we want to know what teams were playing in that match.
+Notice in the that the column matchid in the goal table corresponds to the id column in the game table. We can look up information about game 1012 by finding that row in the game table.
+Show id, stadium, team1, team2 for just game 1012 */
 
 
 
