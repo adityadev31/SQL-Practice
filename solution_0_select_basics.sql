410a5cb71257263f99c6dabb1de3d4e14e68f3e4
AdiTya Dev <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-09-29 10:15:56 +0530

    Update solution_0_select_basics.sql

--- solution_0_select_basics.sql
+++ solution_0_select_basics.sql
@@ -2,11 +2,11 @@
                database ( world )
                
 name	       continent	  area	     population	   gdp
-Afghanistan	 Asia	      652230	   25500100	     20343000000
-Albania	     Europe	    28748	     2831741	     12960000000
-Algeria	     Africa	    2381741	   37100000	     188681000000
-Andorra	     Europe	    468	       78115	       3712000000
-Angola	     Africa	    1246700  	 20609294	     100990000000
+Afghanistan	 Asia	        652230	   25500100	     20343000000
+Albania	     Europe	      28748	     2831741	     12960000000
+Algeria	     Africa	      2381741	   37100000	     188681000000
+Andorra	     Europe	      468	       78115	       3712000000
+Angola	     Africa	      1246700  	 20609294	     100990000000
 
 
 */
@@ -18,3 +18,15 @@ Note that strings (pieces of text that are data) should be in 'single quotes';
 Modify it to show the population of Germany*/
 SELECT population FROM world 
 WHERE name = 'Germany'
+
+
+/* 2 -> Show the name and the population for 'Sweden', 'Norway' and 'Denmark'. */
+SELECT name , population FROM world
+WHERE name IN ('Sweden', 'Norway', 'Denmark')
+
+/* 3 -> Which countries are not too small and not too big? 
+BETWEEN allows range checking (range specified is inclusive of boundary values). 
+The example below shows countries with an area of 250,000-300,000 sq. km. 
+Modify it to show the country and the area for countries with an area between 200,000 and 250,000. */
+SELECT name, area FROM world
+WHERE area BETWEEN 200000 and 250000
