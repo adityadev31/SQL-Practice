bd2aef0e886f4820bf790b89b757ea758d70b864
AdiTya Dev <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-10-01 09:30:23 +0530

    Update solution_7_more_join_operation.sql

--- solution_7_more_join_operation.sql
+++ solution_7_more_join_operation.sql
@@ -76,10 +76,76 @@ WHERE title = 'Casablanca'
 
 what is a cast list?
 Use movieid=11768, (or whatever value you got from the previous question) */
+
+SELECT actor.name FROM actor
+JOIN casting ON casting.actorid = actor.id
+JOIN movie ON movie.id = casting.movieid
+WHERE movie.title = 'Casablanca'
+/*
 SELECT actor.name
 FROM casting
 JOIN actor ON actorid = id
 WHERE movieid = 11768
+*/
 
 
+/* 7 -> Obtain the cast list for the film 'Alien' */
+SELECT actor.name FROM actor
+JOIN casting ON casting.actorid = actor.id
+JOIN movie ON movie.id = casting.movieid
+WHERE movie.title = 'Alien'
+
+
+/* 8 -> List the films in which 'Harrison Ford' has appeared */
+SELECT movie.title FROM movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON casting.actorid = actor.id
+WHERE actor.name =  'Harrison Ford'
+
+
+/* 9 -> List the films where 'Harrison Ford' has appeared - but not in the starring role. [Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role] */
+SELECT movie.title FROM movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON casting.actorid = actor.id
+WHERE actor.name =  'Harrison Ford' AND
+casting.ord != 1
+
+
+/* 10 -> List the films together with the leading star for all 1962 films. */
+SELECT movie.title, actor.name
+FROM movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON casting.actorid = actor.id
+WHERE movie.yr = 1962 AND
+casting.ord = 1
+
+
+/* 11 -> Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies. */
+SELECT movie.yr, COUNT(movie.id)
+FROM movie
+JOIN casting ON casting.movieid = movie.id
+JOIN actor ON actor.id = casting.actorid
+WHERE actor.name = 'Rock Hudson'
+GROUP BY movie.yr
+HAVING COUNT(movie.id) > 2
+
+
+/* 12 -> List the film title and the leading actor for all of the films 'Julie Andrews' played in. */
+SELECT movie.title, actor.name
+FROM movie
+JOIN casting ON casting.movieid = movie.id
+JOIN actor ON actor.id = casting.actorid
+WHERE movie.id IN(
+    SELECT casting.movieid FROM casting
+    JOIN actor ON actor.id = casting.actorid
+    WHERE actor.name = 'Julie Andrews'
+) AND casting.ord = 1
+
+
+/* 13 -> Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles. */
+SELECT actor.name FROM actor
+JOIN casting ON casting.actorid = actor.id
+WHERE casting.ord = 1
+GROUP BY actor.name
+HAVING COUNT(casting.ord)>=15
 
