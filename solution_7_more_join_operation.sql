c6e59be4e1f5c6c158694dec8885add7b38ce1d0
AdiTya Dev <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-10-01 12:15:38 +0530

    Update solution_7_more_join_operation.sql

--- solution_7_more_join_operation.sql
+++ solution_7_more_join_operation.sql
@@ -149,3 +149,15 @@ WHERE casting.ord = 1
 GROUP BY actor.name
 HAVING COUNT(casting.ord)>=15
 
+
+/* 14 -> List the films released in the year 1978 ordered by the number of actors in the cast, then by title. */
+SELECT movie.title, COUNT(casting.actorid) FROM movie
+JOIN casting ON casting.movieid = movie.id
+WHERE movie.yr = 1978
+GROUP BY movie.title
+ORDER BY COUNT(casting.actorid) DESC, movie.title
+
+
+
+/* 15 ->  */
+
